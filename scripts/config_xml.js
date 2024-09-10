4004959eef0dd6247df489a3eeea31c5fadea8d5
Roy Gollub <roy.gollub@sick.de>
2024-09-10 11:22:51 +0200

    Test

--- scripts/config_xml.js
+++ scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2023-04-06 12:49:00 " dc:source="Camtasia,21.0.5,deu" dc:title="SICK_AppSpaceCodingStarterKit_Demo" tscDM:firstFrame="SICK_AppSpaceCodingStarterKit_Demo_First_Frame.png" tscDM:originId="AF4C316C-E6EE-4607-8FE2-D86ADA3526FD" tscDM:project="CSK_Demo">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="599800"/>\
+      <rdf:Description dc:creator="SICK AG" dc:date="2024-09-10 09:40:38 " dc:source="Camtasia,21.0.5,deu" dc:title="SICK_AppSpace_CodingStarterKit_FlowConfig" tscDM:firstFrame="SICK_AppSpace_CodingStarterKit_FlowConfig_First_Frame.png" tscDM:originId="AF4C316C-E6EE-4607-8FE2-D86ADA3526FD" tscDM:project="CSK_FlowConfig_Project">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="207033"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -11,6 +11,14 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
          </tsc:langName>\
          <xmpDM:Tracks>\
             <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="0" xmpDM:duration="38070" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="38070" xmpDM:duration="3360" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="41430" xmpDM:duration="2140" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="43570" xmpDM:duration="1600" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="5" xmpDM:startTime="45170" xmpDM:duration="2100" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="6" xmpDM:startTime="47270" xmpDM:duration="20430" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="7" xmpDM:startTime="67700" xmpDM:duration="9100" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="8" xmpDM:startTime="11000" xmpDM:duration="7930" tscDM:boundingPoly="512,664;1407,664;1407,916;512,916;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://github.com/SICKAppSpaceCodingStarterKit" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="9" xmpDM:startTime="8170" xmpDM:duration="10760" tscDM:boundingPoly="512,291;1407,291;1407,543;512,543;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://www.youtube.com/watch?v=JgjGU0Aws4k" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
             </rdf:Bag>\
          </xmpDM:Tracks>\
          <tscDM:controller>\
@@ -29,7 +37,7 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
             <rdf:Description>\
                <tscDM:files>\
                   <rdf:Seq>\
-                     <rdf:li xmpDM:name="0" xmpDM:value="SICK_AppSpaceCodingStarterKit_Demo.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="SICK_AppSpaceCodingStarterKit_Demo_First_Frame.png"/></rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="SICK_AppSpace_CodingStarterKit_FlowConfig.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="SICK_AppSpace_CodingStarterKit_FlowConfig_First_Frame.png"/></rdf:Seq>\
                </tscDM:files>\
             </rdf:Description>\
          </tscDM:contentList>\
